8d3a82d7c686adb50e8c71a91b9207aac950447a
RichardCzar <64083265+Ricsi1992@users.noreply.github.com>
2024-03-19 15:24:39 +0100

    package structure and db settings created

--- src/main/java/hu/czr/springpontebackend/SpringPonteBackendApplication.java
+++ src/main/java/hu/czr/springpontebackend/SpringPonteBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringPonteBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPonteBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPonteBackendApplication.class, args);
+    }
 
 }
