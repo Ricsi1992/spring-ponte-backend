381e3ae636ed0050b39e05539690ecfc0508a0f7
RichardCzar <64083265+Ricsi1992@users.noreply.github.com>
2024-03-19 14:21:11 +0100

    Initial commit

---
+++ src/main/java/hu/czr/springpontebackend/SpringPonteBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.czr.springpontebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPonteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPonteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/czr/springpontebackend/SpringPonteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.czr.springpontebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPonteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
