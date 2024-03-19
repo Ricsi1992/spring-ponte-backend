252444545f2cda44af235f70e9dd43c7b7211968
RichardCzar <64083265+Ricsi1992@users.noreply.github.com>
2024-03-19 16:05:26 +0100

    pom.xml updated

---
+++ src/main/java/hu/czr/springpontebackend/domain/Image.java
@@ -0,0 +1,24 @@
+package hu.czr.springpontebackend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    private byte[] imageData;
+}
